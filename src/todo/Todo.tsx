4af5b3b32a3709d82af4eba81a0c7dd2ff60b5d7
margaritasing <libiamfreitesm@gmail.com>
2022-11-26 12:16:58 -0300

    primer commit

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Todo = () => {
+  return (
+    <>
+        <h1>Todo: </h1>
+        <ul>
+            <li>todo 1</li>
+            <li>todo 2</li>
+            <li>todo 3</li>
+        </ul>    
+    </>
+  )
+}
